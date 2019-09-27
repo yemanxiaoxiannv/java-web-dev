5a37bd7e7d0ffc0db956d5a27bebbcb9eb7d6f8a
yemanxiaotianshi <2961395022@qq.com>
2019-09-28 03:51:09 +0800

    quickstart模块补推

--- quickstart/src/com/web/LoginServlet.java
+++ quickstart/src/com/web/LoginServlet.java
@@ -19,22 +19,24 @@ public class LoginServlet extends HttpServlet {
     @Override
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-
+       //处理乱码请求
         req.setCharacterEncoding("UTF-8");
-
+//       通过req请求参数获取前台表单的用户名参数
         String username = req.getParameter("username");
+//        获取密码参数
         String password = req.getParameter("password");
-
+//跳转之前把用户名和密码存入req对象
         req.setAttribute("username",username);
         req.setAttribute("password",password);
-
+//通过req跳转，服务器跳转
         req.getRequestDispatcher("/index.jsp").forward(req, resp);
-
+        //通过req请求对象获取session会话对象
         HttpSession session = req.getSession();
-
-        session.setAttribute("username",username);
-        session.setAttribute("password",password);
-        resp.sendRedirect("/index.jsp");
+//把用户名存入session对象
+//        session.setAttribute("username",username);
+//        session.setAttribute("password",password);
+        //通过resp跳转
+//        resp.sendRedirect("/index.jsp");
     }
 }
 
